1a79bfa17d1c35b249985f3596eae3e5216b4bd6
joyciv <gle.demin@gmail.com>
2024-06-10 15:57:51 +0300

    fix pagination - GET params

--- goods/views.py
+++ goods/views.py
@@ -4,23 +4,25 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from goods.models import Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
 
-    if category_slug == 'all':
+    page = request.GET.get("page", 1)
+
+    if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
-        'title': 'Home - каталог',
-        'goods': current_page,
-        'slug_url': category_slug,
+        "title": "Home - каталог",
+        "goods": current_page,
+        "slug_url": category_slug,
     }
 
-    return render(request, 'goods/catalog.html', context)
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request, product_slug):
@@ -28,7 +30,7 @@ def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
 
     context = {
-        'product': product,
+        "product": product,
     }
 
-    return render(request, 'goods/product.html', context=context)
+    return render(request, "goods/product.html", context=context)
