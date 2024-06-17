0c2703595f623316242d5ab4031e94828c00f03a
joyciv <gle.demin@gmail.com>
2024-06-17 17:51:41 +0300

    add registration form and controller

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -27,18 +27,32 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
-        'title': 'Home - регистрация',
+        'title': 'Home - кабинет',
+        'form': form,
     }
     return render(request, 'users/registration.html', context)
 
 
 def profile(request):
+
     context = {
         'title': 'Home - кабинет',
     }
+
     return render(request, 'users/profile.html', context)
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect(reverse('main:index'))
