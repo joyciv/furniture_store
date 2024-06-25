154edc8c9c0b235b57b81394cb9544457ef1a21e
joyciv <gle.demin@gmail.com>
2024-06-25 16:11:59 +0300

    feat admin panel

--- carts/views.py
+++ carts/views.py
@@ -23,10 +23,23 @@ def cart_add(request):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
+    else:
+        carts = Cart.objects.filter(session_key=request.session.session_key, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+
+        else:
+            Cart.objects.create(session_key=request.session.session_key,
+                                product=product,
+                                quantity=1)
+
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
-    )
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
     response_data = {
         "message": "Товар добавлен в корзину",
         "cart_item_html": cart_items_html,
@@ -35,12 +48,45 @@ def cart_add(request):
     return JsonResponse(response_data)
 
 
-def cart_change(request, product_slug):
-    pass
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
 
-
-def cart_remove(request, cart_id):
     cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+    response_data = {
+        "message": "Количество изменено",
+        "cart_item_html": cart_items_html,
+        "quantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
+
+
+def cart_remove(request):
+
+    cart_id = request.POST.get("cart_id")
+
+    cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
 
-    return redirect(request.META['HTTP_REFERER'])
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+    response_data = {
+        "message": "Товар удален",
+        "cart_item_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+
+    return JsonResponse(response_data)
+
+
