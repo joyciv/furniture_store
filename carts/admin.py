71d3db960d38b686db76f45d8b7964d2e77f42c8
joyciv <gle.demin@gmail.com>
2024-06-26 21:24:03 +0300

    add orders app

--- carts/admin.py
+++ carts/admin.py
@@ -7,9 +7,9 @@ from carts.models import Cart
 
 class CartTabAdmin(admin.TabularInline):
     model = Cart
-    fields = ['product', 'quantity', 'created_timestamp', ]
-    search_fields = ['product', 'quantity', 'created_timestamp', ]
-    readonly_fields = ['created_timestamp', ]
+    fields = 'product', 'quantity', 'created_timestamp'
+    search_fields = 'product', 'quantity', 'created_timestamp'
+    readonly_fields = ('created_timestamp',)
     extra = 1
 
 
